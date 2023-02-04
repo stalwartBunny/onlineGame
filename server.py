5a0f72e573cc0c94226b4ba02218f5d1b0118548
stalwartBunny <constructingintelligence@gmail.com>
2023-02-04 10:48:52 -0600

    changed to pickle objects instead of messy str/int conversions

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 from _thread import *
+from player import Player
+import pickle
 
 server = "192.168.1.239"
 port = 5555
@@ -9,52 +11,45 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     s.bind((server, port))
 except socket.error as e:
-    print(e)
+    str(e)
 
 s.listen(2)
-print("Waiting for connection, Server Started")
+print("Waiting for a connection, Server Started")
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
 
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0, 0), (100, 100)]
+players = [Player(0,0,50,50,(255,0,0)), Player(100,100, 50,50, (0,0,255))]
 
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
 
                 print("Received: ", data)
-                print("Sending: ", reply)
+                print("Sending : ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
-
+            conn.sendall(pickle.dumps(reply))
         except:
             break
-    print("Lost connection.")
+
+    print("Lost connection")
     conn.close()
 
 currentPlayer = 0
-
 while True:
     conn, addr = s.accept()
-    print("Connected to", addr)
+    print("Connected to:", addr)
 
     start_new_thread(threaded_client, (conn, currentPlayer))
     currentPlayer += 1
