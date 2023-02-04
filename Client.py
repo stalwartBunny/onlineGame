904d6cf71816835aa7d91b34717fae2fb145e3c6
stalwartBunny <constructingintelligence@gmail.com>
2023-02-03 21:38:08 -0600

    updated for the night

--- Client.py
+++ Client.py
@@ -45,7 +45,7 @@ def read_pos(str):
     return int(str[0]), int(str[1])
 
 def make_pos(tup):
-    return str(tup[0]), str(tup[1])
+    return str(tup[0]) + "," + str(tup[1])
 
 def redrawWindow(win, player, player2):
     win.fill((255, 255, 255))
--- server.py
+++ server.py
@@ -1,38 +1,49 @@
 import socket
 from _thread import *
-import sys
 
 server = "192.168.1.239"
-port  = 5555
+port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     s.bind((server, port))
-
 except socket.error as e:
-    str(e)
+    print(e)
 
 s.listen(2)
 print("Waiting for connection, Server Started")
 
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
 pos = [(0, 0), (100, 100)]
+
 def threaded_client(conn, player):
-    conn.send(str.encode("Connected."))
+    conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
-                print("Received: ", reply)
+                if player == 1:
+                    reply = pos[0]
+                else:
+                    reply = pos[1]
+
+                print("Received: ", data)
                 print("Sending: ", reply)
 
-            conn.sendall(str.encode(reply))
+            conn.sendall(str.encode(make_pos(reply)))
 
         except:
             break
@@ -45,5 +56,5 @@ while True:
     conn, addr = s.accept()
     print("Connected to", addr)
 
-    start_new_thread(threaded_client, (conn, currenPlayer))
+    start_new_thread(threaded_client, (conn, currentPlayer))
     currentPlayer += 1
